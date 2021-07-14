9c23b12f589f97d22446d971551888f6ab417d79
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-14 18:03:37 +0300

    Add JS 2

--- js/script1.js
+++ js/script1.js
@@ -11,8 +11,10 @@ const periodEl = document.querySelector(".card__upper__period");
 const btnEl = document.querySelector(".card__lower__btn");
 
 //  Variables
-let range = undefined;
+let range = 3;
 let yearlyBilling = false;
+let page = 100;
+let price = 16;
 
 const priceList = {
     1: [50, 10],
@@ -22,11 +24,41 @@ const priceList = {
     5: [150, 20],
 };
 
+// FUNCTIONS
+
+function display() {
+    if (yearlyBilling) {
+        price = (price * 12 * 3) / 4;
+        periodEl.innerHTML = "year";
+    } else {
+        periodEl.innerHTML = "month";
+    }
+    pageViewEl.innerHTML = page;
+    priceEl.innerHTML = price.toLocaleString("en-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
+}
+
 console.log(periodCheckboxEl.checked);
 
 periodCheckboxEl.addEventListener("change", () => {
     yearlyBilling = periodCheckboxEl.checked;
+    [page, price] = priceList[range];
+    display();
     console.log(yearlyBilling);
+});
+
+rangeEl.addEventListener("change", () => {
+    range = rangeEl.value;
+
+    console.log(range);
+    console.log(priceList[range]);
+    [page, price] = priceList[range];
+    // if (yearlyBilling) {
+    //     price = price * 12 * 0.75;
+    // }
+    display();
 });
 
 btnEl.addEventListener("click", () => {
